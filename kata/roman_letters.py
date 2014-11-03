42a43e17fa4c94987b3474ff1c24dfbfec13453b
epayet <jakspinning@gmail.com>
2014-11-03 17:32:33 +0100

    Refactoring to do, but working

--- kata/roman_letters.py
+++ kata/roman_letters.py
@@ -1,17 +1,87 @@
 __author__ = 'manu'
 
+steps = [0, 5, 10, 50, 100, 500, 1000]
+
 
 def to_roman(number):
-    roman_start = ["", "V"]
-    current_step = get_current_step(number)
-    roman = roman_start[current_step]
-
-    for i in range(0, number):
-        roman += "I"
-        if len(roman) == 4:
-            roman = "I" + roman_start[current_step+1]
+    isolated_numbers = get_isolated_numbers(number)
+    roman = ""
+    for isolated_number in isolated_numbers:
+        roman += get_isolated_roman(isolated_number)
     return roman
 
+
+def get_isolated_numbers(number):
+    isolated_numbers = []
+    str_number = str(number)
+    nb_digits = len(str_number)
+    for i in range(0, nb_digits):
+        if len(isolated_numbers) != 0:
+            for j in range(0, len(isolated_numbers)):
+                isolated_numbers[j] *= 10
+
+        if str_number[i] != '0':
+            isolated_numbers.append(int(str_number[i]))
+    return isolated_numbers
+
+
+def get_isolated_roman(number):
+    step = get_current_step(number)
+    position = get_number_position_in_step(number)
+    simple_char = get_roman_char(step)
+    roman = ""
+    start_with_5 = is_start_with_5(steps[step])
+
+    if not start_with_5:
+        if position == 4:
+            roman = simple_char + get_roman_char(step + 1)
+        elif position < 4:
+            for i in range(0, position):
+                roman += simple_char
+    else:
+        char_before = get_roman_char(step - 1)
+        if position == 0:
+            return simple_char
+        elif position == 4:
+            roman = char_before + get_roman_char(step + 1)
+        elif position < 4:
+            roman = simple_char
+            for i in range(0, position):
+                roman += char_before
+    return roman
+
+
+def is_start_with_5(number):
+    return str(number).startswith("5")
+
+
+def get_roman_char(step):
+    steps_roman = ["I", "V", "X", "L", "C", "D", "M"]
+    return steps_roman[step]
+
+
+def get_number_position_in_step(number):
+    if number < 5:
+        return number
+    elif number < 10:
+        return number - 5
+    elif number < 50:
+        return number / 10
+    elif number < 100:
+        return (number - 50) / 10
+    elif number < 500:
+        return number / 100
+    elif number < 1000:
+        return (number - 500) / 100
+    else:
+        return number / 1000
+
+
 def get_current_step(number):
-    steps = [0, 5]
-    return 0
+    for step in steps:
+        i = steps.index(step)
+        if step > number:
+            return i - 1
+        elif step == number:
+            return i
+    return len(steps) - 1
--- tests/kata/test_roman_letters.py
+++ tests/kata/test_roman_letters.py
@@ -1,25 +1,129 @@
 __author__ = 'manu'
 
 import unittest
-from roman_letters import to_roman
+from roman_letters import *
 
 
-class RomanLettersTestCase(unittest.TestCase):
+class ToRomanTestCase(unittest.TestCase):
     def test_one(self):
         roman = to_roman(1)
         self.assertEqual("I", roman)
 
-    def test_two(self):
-        roman = to_roman(2)
+    def test_eleven(self):
+        roman = to_roman(11)
+        self.assertEqual("XI", roman)
+
+    def test_complex_number(self):
+        roman = to_roman(1990)
+        self.assertEqual("MCMXC", roman)
+
+    def test_complex_number2(self):
+        roman = to_roman(2008)
+        self.assertEqual("MMVIII", roman)
+
+
+class GetIsolatedNumbersTestCas(unittest.TestCase):
+    def test_get_one(self):
+        isolated = get_isolated_numbers(1)
+        self.assertEqual([1], isolated)
+
+    def test_get_eleven(self):
+        isolated = get_isolated_numbers(11)
+        self.assertEqual([10, 1], isolated)
+
+    def test_get_big_number(self):
+        isolated = get_isolated_numbers(1990)
+        self.assertEqual([1000, 900, 90], isolated)
+
+    def test_get_big_number_2(self):
+        isolated = get_isolated_numbers(2008)
+        self.assertEqual([2000, 8], isolated)
+
+
+class GetIsolatedRomanLetter(unittest.TestCase):
+    def test_get_one(self):
+        roman = get_isolated_roman(1)
+        self.assertEqual("I", roman)
+
+    def test_get_two(self):
+        roman = get_isolated_roman(2)
         self.assertEqual("II", roman)
 
-    def test_four(self):
-        roman = to_roman(4)
+    def test_get_four(self):
+        roman = get_isolated_roman(4)
         self.assertEqual("IV", roman)
 
-    def test_five(self):
-        roman = to_roman(5)
+    def test_get_five(self):
+        roman = get_isolated_roman(5)
         self.assertEqual("V", roman)
+
+    def test_get_seven(self):
+        roman = get_isolated_roman(7)
+        self.assertEqual("VII", roman)
+
+    def test_get_twenty(self):
+        roman = get_isolated_roman(20)
+        self.assertEqual("XX", roman)
+
+    def test_get_thousand(self):
+        roman = get_isolated_roman(1000)
+        self.assertEqual("M", roman)
+
+    def test_get_nine_hundred(self):
+        roman = get_isolated_roman(900)
+        self.assertEqual("CM", roman)
+
+
+class GetNumberPositionInStepTestCase(unittest.TestCase):
+    def test_get_first_step(self):
+        position = get_number_position_in_step(4)
+        self.assertEqual(4, position)
+
+    def test_get_second_step(self):
+        position = get_number_position_in_step(5)
+        self.assertEqual(0, position)
+
+    def test_get_third_step(self):
+        position = get_number_position_in_step(40)
+        self.assertEqual(4, position)
+
+    def test_get_fourth_step(self):
+        position = get_number_position_in_step(70)
+        self.assertEqual(2, position)
+
+    def test_get_fifth_step(self):
+        position = get_number_position_in_step(400)
+        self.assertEqual(4, position)
+
+    def test_get_sixth_step(self):
+        position = get_number_position_in_step(600)
+        self.assertEqual(1, position)
+
+    def test_get_seventh_step(self):
+        position = get_number_position_in_step(3000)
+        self.assertEqual(3, position)
+
+
+class GetCurrentStepTestCase(unittest.TestCase):
+    def test_0(self):
+        step = get_current_step(1)
+        self.assertEqual(0, step)
+
+    def test_1(self):
+        step = get_current_step(7)
+        self.assertEqual(1, step)
+
+    def test_get_last(self):
+        step = get_current_step(2000)
+        self.assertEqual(6, step)
+
+
+class IsStartWith5TestCase(unittest.TestCase):
+    def test_is(self):
+        self.assertEqual(True, is_start_with_5(50))
+
+    def test_is_not(self):
+        self.assertEqual(False, is_start_with_5(1))
 
 
 if __name__ == '__main__':
