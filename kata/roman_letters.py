4ded3bd6c5f4bad67c86ed08c06e92e526bddfdc
epayet <jakspinning@gmail.com>
2014-11-03 13:53:33 +0100

    Work on

--- kata/roman_letters.py
+++ kata/roman_letters.py
@@ -2,7 +2,16 @@ __author__ = 'manu'
 
 
 def to_roman(number):
-    roman = ""
+    roman_start = ["", "V"]
+    current_step = get_current_step(number)
+    roman = roman_start[current_step]
+
     for i in range(0, number):
         roman += "I"
+        if len(roman) == 4:
+            roman = "I" + roman_start[current_step+1]
     return roman
+
+def get_current_step(number):
+    steps = [0, 5]
+    return 0
--- tests/kata/test_roman_letters.py
+++ tests/kata/test_roman_letters.py
@@ -7,11 +7,19 @@ from roman_letters import to_roman
 class RomanLettersTestCase(unittest.TestCase):
     def test_one(self):
         roman = to_roman(1)
-        self.assertEqual(roman, "I")
+        self.assertEqual("I", roman)
 
     def test_two(self):
         roman = to_roman(2)
-        self.assertEqual(roman, "II")
+        self.assertEqual("II", roman)
+
+    def test_four(self):
+        roman = to_roman(4)
+        self.assertEqual("IV", roman)
+
+    def test_five(self):
+        roman = to_roman(5)
+        self.assertEqual("V", roman)
 
 
 if __name__ == '__main__':
